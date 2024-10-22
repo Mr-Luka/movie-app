1e576739eb6c63e29e30e87002d10e232750ac5e
Luka <luka.luka.luka93@gmail.com>
2024-10-22 13:01:21 -0700

    getMoviesApi

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // apikey=ac7d252c
+
+
+async function getMoviesApi (searchTerm) {
+    const URL = `http://www.omdbapi.com/?s=${searchTerm}&apikey=ac7d252c`;
+    const response = await fetch(`${URL}`);
+    const data = await response.json();
+    console.log(data)
+}
+getMoviesApi('Batman')
