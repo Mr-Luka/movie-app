a18d962d4e451dd86bb433fa3d66e35af440324e
Luka <luka.luka.luka93@gmail.com>
2024-10-22 16:21:49 -0700

    displayMoviesList function

--- script.js
+++ script.js
@@ -2,46 +2,48 @@
 
 const searchBox = document.querySelector('#movie-search-box');
 const searchList = document.querySelector('#search-list');
+const resultGrid = document.querySelector('#result-grid')
 
-async function moviesApi (searchTerm) {
-    const response = await fetch(`http://www.omdbapi.com/?s=${searchTerm}&apikey=ac7d252c`);
+async function moviesTermApi(searchTerm) {
+    const response = await fetch( `http://www.omdbapi.com/?s=${searchTerm}&apikey=ac7d252c`)
     const data = await response.json();
-    if (data.Response === 'True') displaySearchMovies(data.Search)
+    if (data.Response === 'True') displayMoviesList(data.Search)
     console.log(data);
 }
-moviesApi('batman');
+moviesTermApi('batman');
 
-function searchMovies() {
+function searchMovies () {
     const search = (searchBox.value).trim();
     if (search.length > 0) {
         searchList.classList.remove('hide-search-list');
-        moviesApi(search);
+        moviesTermApi(search);
     } else {
         searchList.classList.add('hide-search-list');
     }
 }
 
-function displaySearchMovies( movies ) {
-    searchList.innerHTML = '';
-    for (let index = 0; index < movies.length; index++) {
-        let listItem = document.createElement('div');
-        listItem.classList.add('search-list-item');
-        listItem.dataset.id = movies[index].imbID;
+function displayMoviesList (movies) {
+    searchList.innerHTML = "";
+    for (let index = 0; index < movies.length; index++){
+        let searchMovies = document.createElement('div');
+        searchMovies.dataset.id = movies[index].imbID;
+        searchMovies.classList.add('search-list-item');
 
-        if (movies[index].Poster !== 'N/A') {
+        if( movies[index].Poster !== 'N/A') {
             moviePoster = movies[index].Poster;
         } else {
-            moviePoster = `Image_not_found.png`
+            moviePoster = 'Image_not_found.png';
         }
-
-        listItem.innerHTML = `
+        searchMovies.innerHTML = `
         <div class="search-item-thumbnail">
-            <img src="${movies[index].Poster}">
+            <img src="${moviePoster}">
         </div>
         <div class="search-item-info">
             <h3>${movies[index].Title}</h3>
             <p>${movies[index].Year}</p>
-        </div>`;
-        searchList.appendChild(listItem);
+        </div>
+        `
+        searchList.appendChild(searchMovies);
     }
-}
+
+} 
