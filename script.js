5988cf713df4454221357de9aedf431edac65a07
Luka <luka.luka.luka93@gmail.com>
2024-10-22 13:06:44 -0700

    searchMovies function

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 // apikey=ac7d252c
-
-
+const searchBox = document.querySelector('#movie-search-box');
+const searchList = document.querySelector('#search-list')
 async function getMoviesApi (searchTerm) {
     const URL = `http://www.omdbapi.com/?s=${searchTerm}&apikey=ac7d252c`;
     const response = await fetch(`${URL}`);
     const data = await response.json();
     console.log(data)
 }
-getMoviesApi('Batman')
+getMoviesApi('Batman');
+
+function searchMovie (){
+    const searchTerm = (searchBox).value.trim();
+    if( searchTerm !== '') {
+        searchList.classList.remove('hide-search-list');
+        getMoviesApi(searchTerm);
+    } else {
+        searchList.classList.add('hide-search-list');
+    }
+}
